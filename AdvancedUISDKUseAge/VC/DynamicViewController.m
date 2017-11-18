30ced32eb7e50590c783dd5eea6bf57b3ceaf62d
Kingkonglee <konglee873@gmail.com>
2017-11-19 00:18:20 +0800

    save code

--- AdvancedUISDKUseAge/VC/DynamicViewController.m
+++ AdvancedUISDKUseAge/VC/DynamicViewController.m
@@ -8,7 +8,6 @@
 
 typedef void(^ComPletedBlock)(void);
 #import "DynamicViewController.h"
-#import <UIKit/UIKit.h>
 
 @interface DynamicViewController ()<UICollisionBehaviorDelegate>
 {
@@ -33,34 +32,27 @@ typedef void(^ComPletedBlock)(void);
     [s addTarget:self action:@selector(switchAction:) forControlEvents:UIControlEventValueChanged];
     [self.view addSubview:s];
     
-    _imgViewRuiWen = [[UIImageView alloc] initWithFrame:CGRectMake(100, 64, 300, 200)];
+    _imgViewRuiWen = [[UIImageView alloc] initWithFrame:CGRectMake((KWidth - 100)/2, (KHeight - 100)/2 - 200, 100, 100)];
+    _imgViewRuiWen.contentMode = UIViewContentModeScaleAspectFill;
     _imgViewRuiWen.image = [UIImage imageNamed:@"ruiwen.jpg"];
     [self.view addSubview:_imgViewRuiWen];
     
-    _imgViewzlc = [[UIImageView alloc] initWithFrame:CGRectMake(100, KHeight - 200, 80, 120)];
+    _imgViewzlc = [[UIImageView alloc] initWithFrame:CGRectMake(100, KHeight - 200, 100, 100)];
     _imgViewzlc.image = [UIImage imageNamed:@"zlc.jpeg"];
+    _imgViewzlc.contentMode = UIViewContentModeScaleAspectFit;
     [self.view addSubview:_imgViewzlc];
 }
 
 - (void)addAdmation:(void(^)(BOOL isFinish))finishBlock
 {
+    [self addAttachAnimation];
     
-    UIGravityBehavior *gBehavior = [[UIGravityBehavior alloc] initWithItems:@[_imgViewRuiWen,_imgViewzlc]];
-    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-
-    //添加碰撞
-    UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[_imgViewRuiWen,_imgViewzlc]];
-    [collisionBehavior setCollisionMode:UICollisionBehaviorModeBoundaries];
-    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
-    [_animator addBehavior: collisionBehavior];
-    [_animator addBehavior:gBehavior];
-    
-    UIDynamicItemBehavior
     
     if (finishBlock) {
         finishBlock(YES);
     }
 }
+
 - (void)switchAction:(UISwitch *)s
 {
     if (s.on)
@@ -73,8 +65,41 @@ typedef void(^ComPletedBlock)(void);
     }
     else
     {
-        _imgViewRuiWen.frame = CGRectMake(100, 64, 300, 200);
+        _imgViewRuiWen.frame = CGRectMake((KWidth - 100)/2, (KHeight - 100)/2, 100, 100);
     }
+}
+
+- (void)addGravityAnimation
+{
+    UIGravityBehavior *gBehavior = [[UIGravityBehavior alloc] initWithItems:@[_imgViewRuiWen]];
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    NSLog(@" value = %.3f",sqrt(3));
+    //    gBehavior.gravityDirection = CGVectorMake(sqrt(3), 1); // 以1/√3 = tan(a) a = 30°
+    
+    [gBehavior setAngle:90*3.14/180.0];// Angel是弧度，度数 * π/180° = 弧度
+    [gBehavior setMagnitude:8]; //大小，与运动速度有关
+    NSLog(@"dx = %.1f,dy = %.1f",gBehavior.gravityDirection.dx,gBehavior.gravityDirection.dy);
+    /*
+     总结关系:
+     Vector结构体 有两个成员，dx和dy，dy/dx = Magnitude, arctan(dy/dx) = angel弧度制
+     */
+    //添加碰撞
+    UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[_imgViewRuiWen]];
+    [collisionBehavior setCollisionMode:UICollisionBehaviorModeBoundaries];
+    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    [_animator addBehavior: collisionBehavior];
+    [_animator addBehavior:gBehavior];
+
+}
+
+- (void)addAttachAnimation
+{
+    UIAttachmentBehavior *attachBehavior = [[UIAttachmentBehavior alloc] initWithItem:_imgViewRuiWen attachedToAnchor:_imgViewzlc.center];
+    attachBehavior.damping = 0.5;
+    attachBehavior.length = 800;
+    attachBehavior.frequency = 1;
+    [_animator addBehavior:attachBehavior];
 }
 
 
