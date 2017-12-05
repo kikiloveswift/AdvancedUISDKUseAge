e12b13af9a91c39b8f62f4880e84b0edf4e494e7
konglee <konglee873@gmail.com>
2017-12-05 18:39:31 +0800

    save code

--- AdvancedUISDKUseAge/VC/BerKeyFrameVC/BerKeyFrameViewController.m
+++ AdvancedUISDKUseAge/VC/BerKeyFrameVC/BerKeyFrameViewController.m
@@ -10,6 +10,8 @@
 
 @interface BerKeyFrameViewController ()
 
+@property (nonatomic, strong) CAShapeLayer *shape_IMGLayer;
+
 @end
 
 @implementation BerKeyFrameViewController
@@ -17,9 +19,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self initUILayer];
+}
+
+- (void)initUILayer
+{
+    UIImage *img = [UIImage imageNamed:@"zlc.jpg"];
+    _shape_IMGLayer = [CAShapeLayer layer];
+    _shape_IMGLayer.frame = CGRectMake(20, 150, 200, 0);
+    _shape_IMGLayer.contents = (__bridge id _Nullable)(img.CGImage);
+    [self.view.layer addSublayer:_shape_IMGLayer];
+    
 }
 - (IBAction)beginAnimation:(UIButton *)sender
 {
+    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"height"];
+    basicAnimation.toValue = @300;
+    basicAnimation.fromValue = @0;
+    basicAnimation.duration = 3.0f;
+    [_shape_IMGLayer addAnimation:basicAnimation forKey:@"animation"];
+    
     
 }
 
