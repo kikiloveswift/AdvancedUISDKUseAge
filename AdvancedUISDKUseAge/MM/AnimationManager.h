83a02c4f0c076233f21a2b9106ba947a56b41ccb
konglee <konglee873@gmail.com>
2017-12-10 18:14:09 +0800

    显式引用

--- AdvancedUISDKUseAge/MM/AnimationManager.h
+++ AdvancedUISDKUseAge/MM/AnimationManager.h
@@ -27,4 +27,40 @@ namespace AnimationManager
     {
         std::cout<< GetMax(t1, t2)<< std::endl;
     }
+    
+    typedef int (*fun) (int a);
+    typedef int AFun (int b);
+    fun funC;
+    
+    int funB(int b)
+    {
+        return b;
+    }
+    void getValue(void)
+    {
+        funC = funB;
+        int c = (*funC)(10);
+        std::cout << "c = " << c <<std::endl;
+        AFun *funD = funB;
+        int d = funD(32);
+        std::cout << "d = " << d <<std::endl;
+    }
+    
+    struct job
+    {
+        char name[40];
+        int age;
+        double salary;
+    };
+    template <> void DisplayResult<job>(job& j1, job& j2)
+    {
+        double temp1;
+        int age1;
+        age1 = j1.age;
+        temp1 = j1.salary;
+        j1.age = j2.age;
+        j1.salary = j2.salary;
+        j2.age = age1;
+        j2.salary = temp1;
+    }
 }
