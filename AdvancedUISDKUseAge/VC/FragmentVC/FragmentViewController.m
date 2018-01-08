c4a0a0a4ff6187679ce8fe36ed2cad2fb4317904
konglee <konglee873@gmail.com>
2018-01-08 17:59:30 +0800

    save code
    更新代码

--- AdvancedUISDKUseAge/VC/FragmentVC/FragmentViewController.m
+++ AdvancedUISDKUseAge/VC/FragmentVC/FragmentViewController.m
@@ -22,7 +22,49 @@ typedef void (^FinishAnimationBlock)(BOOL isFinished);
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self initUI];
+//    [self initUI];
+    [self initLayer];
+}
+
+
+- (void)initLayer
+{
+    self.view.backgroundColor = [UIColor blackColor];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction:)];
+    [self.view addGestureRecognizer:tap];
+}
+
+- (void)tapAction:(UITapGestureRecognizer *)tap
+{
+    CALayer *slayer = [CALayer layer];
+    slayer.frame = CGRectMake(0, 64, KWidth, 400);
+    slayer.backgroundColor = [UIColor colorWithHexString:@"#FFE81F"].CGColor;
+    [self.view.layer addSublayer:slayer];
+    
+    UIBezierPath *bPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(KWidth/2, 100) radius:100 startAngle:0 endAngle:M_PI * 2 clockwise:YES];
+    CAShapeLayer *maskLayer = [CAShapeLayer layer];
+    maskLayer.path = bPath.CGPath;
+    slayer.mask = maskLayer;
+    
+    UIBezierPath *beginPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(KWidth/2, 100) radius:0.1 startAngle:0 endAngle:M_PI * 2 clockwise:YES];
+    
+    UIBezierPath *endPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(KWidth/2, 100) radius:100 startAngle:0 endAngle:M_PI * 2 clockwise:YES];
+    maskLayer.path = beginPath.CGPath;
+    [beginPath setUsesEvenOddFillRule:YES];
+    maskLayer.fillMode = kCAFillRuleEvenOdd;
+    CAKeyframeAnimation *keyAnimation = [CAKeyframeAnimation animationWithKeyPath:@"path"];
+    keyAnimation.values = @[(__bridge id)beginPath.CGPath, (__bridge id)endPath.CGPath];
+    keyAnimation.duration = 0.2f;
+    keyAnimation.beginTime = CACurrentMediaTime();
+    keyAnimation.delegate = self;
+    [maskLayer addAnimation:keyAnimation forKey:@"pathAnimation"];
+    self.finishBlock = ^(BOOL isFinished) {
+        CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+        basicAnimation.fromValue = @1;
+        basicAnimation.toValue = @0;
+        basicAnimation.duration = 0.2;
+        [slayer addAnimation:basicAnimation forKey:@"opacityL"];
+    };
 }
 
 - (void)initUI
@@ -41,13 +83,6 @@ typedef void (^FinishAnimationBlock)(BOOL isFinished);
     dlayer.frame = CGRectMake((KWidth - 200)/2, 200, 200, 50);
     dlayer.backgroundColor = [UIColor blueColor].CGColor;
     [self.view.layer addSublayer:dlayer];
-    
-//    CALayer *maskLayer = [CALayer layer];
-//    maskLayer.frame = CGRectMake(0, 0, 260, 10);
-//    maskLayer.backgroundColor = [UIColor greenColor].CGColor;
-//    maskLayer.opacity = 0.5;
-//    dlayer.mask = maskLayer;
-//    dlayer.masksToBounds = NO;
 }
 
 - (void)btnClick:(UIButton *)btn
@@ -60,27 +95,27 @@ typedef void (^FinishAnimationBlock)(BOOL isFinished);
     [btn.layer insertSublayer:fillLayer atIndex:0];
     
     CGPoint center = btn.center;
-    CGFloat radius = MAX(btn.bounds.size.width/2, btn.bounds.size.height/2);
-    CGRect rect = {.origin= {center.x, center.y}, .size = {0.1,0.1} };
-    CGRect begininsetRect = CGRectInset(rect, 100, 25);
-    CGRect endinsetRect =CGRectMake(0, 0, 100, 100);
+    CGRect rect = {.origin= {center.x, center.y}, .size = {0,0} };
+    CGRect begininsetRect = CGRectInset(rect, 0, 0);
+    CGRect endinsetRect = CGRectInset(rect, -50, -50);
 
 //    CGRect endinsetRect = btn.frame;
-//    CGPathRef startPath = CGPathCreateWithEllipseInRect(begininsetRect, nil);
-//    CGPathRef endPath = CGPathCreateWithEllipseInRect(endinsetRect, nil);
-    UIBezierPath *beginPath = [UIBezierPath bezierPathWithArcCenter:btn.center radius:1 startAngle:0 endAngle:M_PI *2 clockwise:YES];
-    UIBezierPath *terminalPath = [UIBezierPath bezierPathWithArcCenter:btn.center radius:100 startAngle:0 endAngle:M_PI *2 clockwise:YES];
     
-    dlayer.path = terminalPath.CGPath;
-
-    CGPathRef startPath = beginPath.CGPath;
-    CGPathRef endPath = terminalPath.CGPath;
+    CGPathRef startPath = CGPathCreateWithRoundedRect(begininsetRect, 0, 0, nil);
+    CGPathRef endPath = CGPathCreateWithRoundedRect(endinsetRect, 25, 25, nil);
+//    UIBezierPath *beginPath = [UIBezierPath bezierPathWithArcCenter:btn.center radius:1 startAngle:0 endAngle:M_PI *2 clockwise:YES];
+//    UIBezierPath *terminalPath = [UIBezierPath bezierPathWithArcCenter:btn.center radius:100 startAngle:0 endAngle:M_PI *2 clockwise:YES];
+    
+//    dlayer.path = terminalPath.CGPath;
+//
+//    CGPathRef startPath = beginPath.CGPath;
+//    CGPathRef endPath = terminalPath.CGPath;
     
     CAShapeLayer *maskLayer = [CAShapeLayer layer];
     maskLayer.path = startPath;
-    maskLayer.fillMode = kCAFillRuleEvenOdd;
+//    maskLayer.fillMode = kCAFillRuleEvenOdd;
     fillLayer.mask = maskLayer;
-    maskLayer.frame = btn.bounds;
+//    maskLayer.frame = btn.bounds;
     CABasicAnimation *pathAnimation = [CABasicAnimation animationWithKeyPath:@"path"];
     pathAnimation.duration = 0.2f;
     pathAnimation.delegate = self;
