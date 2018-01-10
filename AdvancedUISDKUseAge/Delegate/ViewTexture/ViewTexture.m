b68dce56ee827fbcbfcf2495233a9edbf110802f
konglee <konglee873@gmail.com>
2018-01-10 21:27:34 +0800

    save code

--- AdvancedUISDKUseAge/Delegate/ViewTexture/ViewTexture.m
+++ AdvancedUISDKUseAge/Delegate/ViewTexture/ViewTexture.m
@@ -31,14 +31,46 @@
     return self;
 }
 
+- (void)dealloc
+{
+    if (name != 0)
+    {
+        glDeleteTextures(1, &name);
+    }
+}
 - (void)setUPOpenGL
 {
     glGenTextures(1, &name);
-    glBindTexture(GL_TEXTURE_2D, name);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindTexture((GLenum)GL_TEXTURE_2D, name);
+    glTexParameteri((GLenum)GL_TEXTURE_2D, (GLint)GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri((GLenum)GL_TEXTURE_2D, (GLenum)GL_TEXTURE_MAG_FILTER, (GLint)GL_LINEAR);
+    glTexParameteri((GLenum)GL_TEXTURE_2D, (GLenum)GL_TEXTURE_WRAP_S, (GLint)GL_CLAMP_TO_EDGE);
+    glTexParameteri((GLenum)GL_TEXTURE_2D, (GLenum)GL_TEXTURE_WRAP_T, (GLint)GL_CLAMP_TO_EDGE);
+    glBindTexture((GLenum)GL_TEXTURE_2D, 0);
 }
+
+- (void)renderView:(UIView *)view
+{
+    CGFloat scale = [UIScreen mainScreen].scale;
+    width = (GLsizei)(view.layer.bounds.size.width * scale);
+    height = (GLsizei)(view.layer.bounds.size.height * scale);
+    NSMutableArray *textureMarrs = [[NSMutableArray alloc] initWithCapacity:(width * height *4)];
+    for (int i = 0; i < textureMarrs.count; i ++)
+    {
+        textureMarrs[i] = @((GLubyte)0);
+    }
+    CGColorSpaceRef colorSpace =  CGColorSpaceCreateDeviceRGB();
+    void *data = (__bridge void*)textureMarrs;
+    
+    CGContextRef context = CGBitmapContextCreate(data, (int)width, (int)height, 8, (int)width *4, colorSpace, (uint)kCGImageAlphaPremultipliedLast|(uint)kCGBitmapByteOrder32Big);
+    CGContextScaleCTM(context, scale, scale);
+    UIGraphicsPushContext(context);
+    [view drawViewHierarchyInRect:view.layer.bounds afterScreenUpdates:false];
+    UIGraphicsPopContext();
+    
+    glBindTexture((GLenum)GL_TEXTURE_2D, name);
+    glTexImage2D((GLenum)GL_TEXTURE_2D, 0, (GLint)GL_RGBA, width, height, 0, (GLenum)GL_RGBA, (GLenum)GL_UNSIGNED_BYTE, data);
+    glBindTexture((GLenum)GL_TEXTURE_2D, 0);
+}
+
 @end
